74326e0be4bdb158af3223ccd406c853cdba1146
Wenard Santos <wenardken57@gmail.com>
2025-02-07 14:50:29 +0800

    Create function to get and evaluate player choice

--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
 /*
+  Rock, paper, scissors against computer in console.
   Game will be played agains computer that returns random decision
 */
 
+function getPlayerChoice() {
+  
+  let choice = parseInt(prompt("1. Rock, 2. Paper, 3. Scissor"));
+
+  if (isNaN(choice)) {
+    console.log("Please enter a valid number (1-3 only)");
+    return;
+  } else {
+    switch (choice) {
+      case 1:
+        return "rock";
+      case 2:
+        return "paper";
+      case 3:
+        return "scissor";
+      default:
+        console.log("Please enter a valid number (1-3 only)");
+        return;
+    }
+  }
+}
 
 // returns random "rock", "paper", or "scissors"
 function getComputerChoice() {
@@ -17,4 +39,4 @@ function getComputerChoice() {
   }
 }
 
-console.log(getComputerChoice());
+console.log(getPlayerChoice());
