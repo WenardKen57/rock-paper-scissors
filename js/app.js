d9c520fd54a6ef1cbf00ae9d0f1150875af4ef9e
Wenard Santos <wenardken57@gmail.com>
2025-02-07 14:58:37 +0800

    Wrap process of evaluating choice serpate function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@
   Game will be played agains computer that returns random decision
 */
 
-function getPlayerChoice() {
-  
-  let choice = parseInt(prompt("1. Rock, 2. Paper, 3. Scissor"));
+let playerScore = 0;
+let computerScore = 0;
 
+function evaluateChoice(choice) {
   if (isNaN(choice)) {
+    // Player enters letter
     console.log("Please enter a valid number (1-3 only)");
     return;
   } else {
@@ -19,24 +20,22 @@ function getPlayerChoice() {
       case 3:
         return "scissor";
       default:
+        // Player enters a number below or beyond the required choice
         console.log("Please enter a valid number (1-3 only)");
         return;
     }
   }
 }
 
+function getPlayerChoice() {
+  let choice = parseInt(prompt("1. Rock, 2. Paper, 3. Scissor"));
+
+  return evaluateChoice(choice);
+}
+
 // returns random "rock", "paper", or "scissors"
 function getComputerChoice() {
-  switch (Math.floor(Math.random() * 3 + 1)) {
-    case 1:
-      return "rock";
-    case 2:
-      return "paper";
-    case 3:
-      return "scissor";
-    default:
-      break;
-  }
+  return evaluateChoice(Math.floor(Math.random() * 3 + 1));
 }
 
 console.log(getPlayerChoice());
